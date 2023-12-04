5d016c14552d22a5b4ab24d8c69f6db01c9067d8
zyhou <richard.maxime9@gmail.com>
2023-12-04 09:31:15 +0100

    add day 4 solution

---
+++ 04/index.ts
@@ -0,0 +1,47 @@
+import { Expect, Equal } from "type-testing";
+
+interface Address {
+  address: string;
+  city: string;
+}
+type PresentDeliveryList<T> = {
+  [K in keyof T]: Address
+}
+
+// ------------------- Test section ---------------------
+
+interface MixedBehaviorList {
+  john: { behavior: "good" };
+  jimmy: { behavior: "bad" };
+  sara: { behavior: "good" };
+  suzy: { behavior: "good" };
+  chris: { behavior: "good" };
+  penny: { behavior: "bad" };
+}
+type test_MixedBehaviorTest_actual = PresentDeliveryList<MixedBehaviorList>;
+//   ^?
+interface test_MixedBehaviorTest_expected {
+  john: Address;
+  jimmy: Address;
+  sara: Address;
+  suzy: Address;
+  chris: Address;
+  penny: Address;
+}
+type test_MixedBehaviorTest = Expect<
+  Equal<test_MixedBehaviorTest_actual, test_MixedBehaviorTest_expected>
+>;
+
+interface Unrelated {
+  hello: { hello: "hello" };
+  world: { world: "world" };
+}
+type test_Unrelated_actual = PresentDeliveryList<Unrelated>;
+//   ^?
+interface test_Unrelated_expected {
+  hello: Address;
+  world: Address;
+}
+type test_Unrelated = Expect<
+  Equal<test_Unrelated_actual, test_Unrelated_expected>
+>;
