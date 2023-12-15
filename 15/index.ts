e3cd869765a261679aa309ee013ea4721445c23f
zyhou <richard.maxime9@gmail.com>
2023-12-15 09:43:51 +0100

    addd day 15

---
+++ 15/index.ts
@@ -0,0 +1,28 @@
+import { Expect, Equal } from "type-testing";
+
+// Same as my day 13 solution
+// It used Distributive Conditional Types
+// https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
+type BoxToys<
+  Word extends string,
+  Length,
+  Accumulator extends string[] = [],
+> = Length extends Accumulator["length"]
+  ? Accumulator
+  : BoxToys<Word, Length, [...Accumulator, Word]>;
+
+// ------------------- Test section ---------------------
+
+type test_doll_actual = BoxToys<"doll", 1>;
+//   ^?
+type test_doll_expected = ["doll"];
+type test_doll = Expect<Equal<test_doll_expected, test_doll_actual>>;
+
+type test_nutcracker_actual = BoxToys<"nutcracker", 3 | 4>;
+//   ^?
+type test_nutcracker_expected =
+  | ["nutcracker", "nutcracker", "nutcracker"]
+  | ["nutcracker", "nutcracker", "nutcracker", "nutcracker"];
+type test_nutcracker = Expect<
+  Equal<test_nutcracker_expected, test_nutcracker_actual>
+>;
